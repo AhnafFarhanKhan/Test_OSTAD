6dc691f7ac71ccac782580f655f3d58ec1de1c51
AhnafFarhanKhan <ahnaffarhankhan@gmail.com>
2026-01-06 23:35:56 -0700

    changes in main

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from dotenv import load_dotenv
 #Load environment variables from .env file
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 print(f"Your API Key is: {API_KEY}")
+print(f"Your Database URL is: {DATABASE_URL}")
